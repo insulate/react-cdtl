03df3cd91e87d4987b1d0cda85bb98d70501701f
insulate <biiwzmethom@gmail.com>
2021-12-13 23:11:41 +0700

    react query

--- src/App.js
+++ src/App.js
@@ -10,33 +10,37 @@ import AboutPage from "./pages/AboutPage";
 import ProductPage from "./pages/ProductPage";
 import DetailPage from "./pages/DetailPage"
 import HospitalPage from "./pages/hospital/HospitalPage";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient()
+
 function App() {
   return (
     <>
-
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          {/* exact ให้ใส้เฉพาะหน้าแรก */}
-          <Route exact path="/" element={<HomePage />} />
-          <Route path="about" element={<AboutPage />} />
-          <Route path="product" element={<ProductPage />} />
-          <Route path="detail/:id/title/:title" element={<DetailPage />} />
-          <Route path="hospital" element={<HospitalPage />} />
-          <Route
-            path="*"
-            element={
-              <main style={{ padding: "1rem" }}>
-                <p>There's nothing here!</p>
-              </main>
-            }
-          />
-        </Routes>
-        <footer className="container">
-          <p>© Company 2017-2021</p>
-        </footer>
-      </BrowserRouter>
-
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            {/* exact ให้ใส้เฉพาะหน้าแรก */}
+            <Route exact path="/" element={<HomePage />} />
+            <Route path="about" element={<AboutPage />} />
+            <Route path="product" element={<ProductPage />} />
+            <Route path="detail/:id/title/:title" element={<DetailPage />} />
+            <Route path="hospital" element={<HospitalPage />} />
+            <Route
+              path="*"
+              element={
+                <main style={{ padding: "1rem" }}>
+                  <p>There's nothing here!</p>
+                </main>
+              }
+            />
+          </Routes>
+          <footer className="container">
+            <p>© Company 2017-2021</p>
+          </footer>
+        </BrowserRouter>
+      </QueryClientProvider>
     </>
   );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,49 @@
 import React from 'react'
+import { useQuery } from 'react-query'
+import { Spinner } from 'react-bootstrap'
 
 const HomePage = () => {
+
+    // const { isLoading, error, data, isFetching } = useQuery('getData', () =>
+    //     fetch('https://api.codingthailand.com/api/news?page=1').then(res =>
+    //         res.json()
+    //     )
+    // )
+    const query = useQuery("getData", () => {
+        const controller = new AbortController();
+        const signal = controller.signal;
+
+        const promise = fetch(
+            "https://api.codingthailand.com/api/news?page=1", {
+            method: 'get',
+            signal: signal
+        }
+        ).then((res) => res.json())
+
+        //cancel request
+        promise.cancel = () => controller.abort();
+
+        return promise;
+    })
+
+    const { isLoading, error, data, isFetching } = query;
+
+    if (isLoading === true) {
+        return (
+            <div className='text-center mt-5'>
+                <Spinner animation="border" variant="primary" />
+            </div>
+        )
+    }
+
+    if (error) {
+        return (
+            <div className='text-center mt-5'>
+                <p >{JSON.stringify(error)}</p>
+            </div>
+        )
+    }
+
     return (
         <>
             <main role="main">
@@ -15,21 +58,17 @@ const HomePage = () => {
                 <div className="container">
                     {/* Example row of columns */}
                     <div className="row">
-                        <div className="col-md-4">
-                            <h2>Heading</h2>
-                            <p>Will you do the same for me? It's time to face the music I'm no longer your muse. Heard it's beautiful, be the judge and my girls gonna take a vote. I can feel a phoenix inside of me. Heaven is jealous of our love, angels are crying from up above. Yeah, you take me to utopia.</p>
-                            <p><button className="btn btn-secondary">View details »</button></p>
-                        </div>
-                        <div className="col-md-4">
-                            <h2>Heading</h2>
-                            <p>Standing on the frontline when the bombs start to fall. Heaven is jealous of our love, angels are crying from up above. Can't replace you with a million rings. Boy, when you're with me I'll give you a taste. There’s no going back. Before you met me I was alright but things were kinda heavy. Heavy is the head that wears the crown.</p>
-                            <p><button className="btn btn-secondary">View details »</button></p>
-                        </div>
-                        <div className="col-md-4">
-                            <h2>Heading</h2>
-                            <p>Playing ping pong all night long, everything's all neon and hazy. Yeah, she's so in demand. She's sweet as pie but if you break her heart. But down to earth. It's time to face the music I'm no longer your muse. I guess that I forgot I had a choice.</p>
-                            <p><button className="btn btn-secondary">View details »</button></p>
-                        </div>
+                        {
+                            data.data.map((news, index) => {
+                                return (
+                                    <div className="col-md-4">
+                                        <h2>{news.topic}</h2>
+                                        <p>{news.detail}</p>
+                                        <p>หมวดหมู่: {news.name}</p>
+                                    </div>
+                                )
+                            })
+                        }
                     </div>
                     <hr />
                 </div> {/* /container */}
